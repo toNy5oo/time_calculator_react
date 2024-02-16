c21b1d148c7968bb16a41c63e45563b025a3a3ba
toNy5oo <lindirizzoemail@gmail.com>
2024-02-16 11:29:35 +0100

    Prettier

--- src/utils/timeHelper.js
+++ src/utils/timeHelper.js
@@ -1,25 +1,28 @@
 import dayjs from "dayjs";
+import { HourAndMinFormat } from "../components/assets/const/const";
 
 /** The initial minutes need to be a multiplier of 5
  * The function calculates the closest interval to the actual time and returns
  * a dayjs object with that minutes set.
  */
 export const calculateInitalTime = () => {
-	const now = dayjs();
-	const minutes = dayjs().minute();
-	const mutiplier = parseInt(minutes / 5);
-	now.minute(now.minute() - mutiplier);
-	return dayjs(dayjs().minute(5 * mutiplier), "HH:mm");
-	// return now.format("DD/MM/YYYY HH:mm");
+  const now = dayjs();
+  const minutes = dayjs().minute();
+  const multiplier = parseInt(minutes / 5);
+  now.minute(now.minute() - multiplier);
+  return dayjs(dayjs().minute(5 * multiplier), HourAndMinFormat);
+  // return now.format("DD/MM/YYYY HH:mm");
 };
 
 /** Calculate time played in seconds and rounds up the number divided by 60 */
 export const timePlayed = (start, end) => {
-	let startTime = dayjs(start, "DD-MM-YYYY hh:mm");
-	let endTime = dayjs(end, "DD-MM-YYYY hh:mm");
+  let startTime = dayjs(start, "DD-MM-YYYY hh:mm");
+  let endTime = dayjs(end, "DD-MM-YYYY hh:mm");
 
-	let minutesDiff = Math.floor(endTime.diff(startTime, "minute", true));
-	return minutesDiff;
+  let minutesDiff = Math.floor(
+    endTime.diff(startTime, "minute", true)
+  );
+  return minutesDiff;
 };
 
 /**
@@ -28,15 +31,18 @@ export const timePlayed = (start, end) => {
  * @returns
  */
 export function parseTime(minutesPlayed) {
-	let hours = Math.floor(minutesPlayed / 60).toLocaleString("en-US", {
-		minimumIntegerDigits: 2,
-		useGrouping: false,
-	});
-	let minutes = Math.ceil(minutesPlayed % 60).toLocaleString("en-US", {
-		minimumIntegerDigits: 2,
-		useGrouping: false,
-	});
-	return `${hours}h : ${minutes}m`;
+  let hours = Math.floor(minutesPlayed / 60).toLocaleString("en-US", {
+    minimumIntegerDigits: 2,
+    useGrouping: false,
+  });
+  let minutes = Math.ceil(minutesPlayed % 60).toLocaleString(
+    "en-US",
+    {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    }
+  );
+  return `${hours}h : ${minutes}m`;
 }
 
 export const isNotZero = (time) => time !== 0;
