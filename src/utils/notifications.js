36dddc97518fb7924626c76753ea53cd069b7132
toNy5oo <lindirizzoemail@gmail.com>
2024-04-19 12:54:18 +0200

    Add general notification

--- src/utils/notifications.js
+++ src/utils/notifications.js
@@ -1,17 +1,25 @@
 import { notification } from "antd";
 
 export const notifyOnClose = (num) => {
-  notification.success({
-    type: "success",
-    message: `Tisch ${num} ist jetzt nochmal frei`,
-    description:
-      "The table has been closed and now is available again to be rented.",
-  });
+    notification.success({
+        type: "success",
+        message: `Tisch ${num} ist jetzt nochmal frei`,
+        description:
+            "The table has been closed and now is available again to be rented.",
+    });
 };
 
 export const notifyOnHold = (_, message, desc) => {
-  notification.success({
-    message: `${message}`,
-    description: `${desc}`,
-  });
+    notification.success({
+        message: `${message}`,
+        description: `${desc}`,
+    });
+};
+
+//Notification
+export const showNotification = (type, title, msg) => {
+    notification[type]({
+        message: title,
+        description: msg,
+    });
 };
