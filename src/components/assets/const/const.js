ccc1777a8750192bf7b5da4a3e4d404b950d3508
toNy5oo <lindirizzoemail@gmail.com>
2025-09-02 13:22:06 +0200

    Update const.js

--- src/components/assets/const/const.js
+++ src/components/assets/const/const.js
@@ -1,5 +1,5 @@
-export const FULL_PRICE = 12;
-export const HALF_PRICE = FULL_PRICE / 2;
+export const FULL_PRICE = 14;
+export const HALF_PRICE = 10; //FULL_PRICE / 2;
 export const FULL_PRICE_MIN = parseFloat(FULL_PRICE / 60);
 export const HALF_PRICE_MIN = parseFloat(HALF_PRICE / 60);
 
