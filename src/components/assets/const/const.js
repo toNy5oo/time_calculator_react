a405a321d51b4fdffd7375228f1135a34b2a2146
toNy5oo <lindirizzoemail@gmail.com>
2025-09-03 21:40:52 +0200

    Changed constant to more explicit name

--- src/components/assets/const/const.js
+++ src/components/assets/const/const.js
@@ -1,12 +1,12 @@
 export const FULL_PRICE = 14;
 export const HALF_PRICE = 10; //FULL_PRICE / 2;
-export const FULL_PRICE_MIN = parseFloat(FULL_PRICE / 60);
-export const HALF_PRICE_MIN = parseFloat(HALF_PRICE / 60);
+export const FULL_PRICE_PER_MINUTES = parseFloat(FULL_PRICE / 60);
+export const HALF_PRICE_PER_MINUTES = parseFloat(HALF_PRICE / 60);
 
 export const HourAndMinFormat = "HH:mm";
 
 export const navBarItems = [
-  { linkTo: "/", label: "Timer" },
-  { linkTo: "/getranke", label: "Getränke" },
-  { linkTo: "/reservierungen", label: "Reservierung" },
+    { linkTo: "/", label: "Timer" },
+    { linkTo: "/getranke", label: "Getränke" },
+    { linkTo: "/reservierungen", label: "Reservierung" },
 ];
